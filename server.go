c3b143f44a08b70df680a0aed938c79d87b026a9
ashutosh5786 <ashutosh5786@outlook.com>
2025-04-07 16:21:20 +0100

    simplfiying the get and post logic // This is a combination of 1 or more commits. // The first commit's message is: // simplfiying the get and post logic

--- server.go
+++ server.go
@@ -18,12 +18,24 @@ func postjobs(w http.ResponseWriter, r *http.Request) {
 
 
 func main() {
-	fmt.Printf("Starting Server on http://localhost:8080\n")
-
+	
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, World!")
 	})
 
+	http.HandleFunc("/job", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			getjobs(w, r)
+		} else if r.Method == http.MethodPost {
+			postjobs(w, r)
+		} else {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+	})
+	
+
+	fmt.Printf("Starting Server on http://localhost:8080\n")
 	// Start the server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Printf("Error starting server: %s\n", err)
