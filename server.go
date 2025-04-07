ec0213c10f29d2af31cfdef886a08b6fea35c941
ashutosh5786 <ashutosh5786@outlook.com>
2025-04-08 00:01:22 +0100

    done with get job added TODO

--- server.go
+++ server.go
@@ -1,20 +1,38 @@
 package main
 
 import (
-    "log"
-    "github.com/gofiber/fiber/v3"	
+	"log"
+	"github.com/gofiber/fiber/v3"
 )
 
+// Job struct to represent a job
+type Job struct {
+	jobId int    `json:"jobId"`
+	job   string `json:"job"`
+}
+
+// JobList struct to represent a list of jobs. Making a Slice as it can grow in size depending upon the struct job which is declate above
+type JobList struct {
+	Jobs []Job `json:"jobs"`
+}
+
+var jobList = JobList{
+	Jobs: []Job{},
+}
+
 func main() {
-    // Initialize a new Fiber app
-    app := fiber.New()
+	// Initialize a new Fiber app
+	app := fiber.New()
 
-    // Define a route for the GET method on the root path '/'
-    app.Get("/", func(c fiber.Ctx) error {
-        // Send a string response to the client
-        return c.SendString("Hello, World 👋!")
-    })
+	// Set up a simple GET endpoint to return the job list TODO Needed to get the data from Any DB
+	app.Get("/job", func(c fiber.Ctx) error {
+		return c.JSON(jobList)
+	})
 
-    // Start the server on port 3000
-    log.Fatal(app.Listen(":8080"))
+	app.Post("/job", func(c fiber.Ctx) error {
+		return c.SendString("Job Post API")
+	})
+
+	// Start the server on port 80 make the docker compose to serve on port 8080 TODO Dont Forget about it in the end
+	log.Fatal(app.Listen(":80"))
 }
