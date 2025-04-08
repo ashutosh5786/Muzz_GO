3dd5a48edda4c058a1f59b03f7e14708de4f3554
ashutosh5786 <ashutosh5786@outlook.com>
2025-04-08 15:45:05 +0100

    started to work on GET /Job

--- server.go
+++ server.go
@@ -4,33 +4,25 @@ import (
 	"context"
 	"log"
 	"os"
-	"fmt"
+	"time"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 	"go.mongodb.org/mongo-driver/v2/mongo/readpref"
 )
 
 // Job struct to represent a job
-// TODO Needed to generate a unique jobId for each job. For now, we are using a simple int
-// TODO Needed to change the jobId to string and use UUID for unique jobId generated automatically
 type Job struct {
-	JobId string `json:"jobId" bson:"jobId"`
-	Job   string `json:"job" bson:"job"`
+	JobId     string    `json:"jobId" bson:"jobId"`
+	Job       string    `json:"job" bson:"job"`
+	CreatedAt time.Time `json:"createdAt" bson:"createdAt"`
 }
 
 var jobCollection *mongo.Collection
-
-// JobList struct to represent a list of jobs. Making a Slice as it can grow in size depending upon the struct job which is declate above
-// type JobList struct {
-// 	Jobs []Job `json:"jobs"`
-// }
-
-// var jobList = JobList{
-// 	Jobs: []Job{},
-// }
 
 func main() {
 	// Load environment variables from .env file
@@ -56,21 +48,25 @@ func main() {
 	}
 	log.Println("Connected to MongoDB")
 
-	jobCollection := client.Database("jobDB").Collection("jobs")
+	jobCollection = client.Database("jobDB").Collection("jobs")
 
 	// Initialize a new Fiber app
 	app := fiber.New()
 
-	// app.Get("/job", func(c *fiber.Ctx) error {
-	// 	amount, checkpoint := c.Query("amount"), c.Query("checkpoint")
+	// Get /Job Endpoint to retrive the jobs
+	app.Get("/job", func(c *fiber.Ctx) error {
+		amount, checkpoint := c.Query("amount"), c.Query("checkpoint")
 
-	// 	// REMOVE IT AFTER TESTING
-	// 	log.Printf("Amount: %s, Checkpoint: %s", amount, checkpoint)
+		jobCollection.Indexes().CreateOne(context.TODO(), mongo.IndexModel{
+			Keys: bson.D{{Key: "createdAt", Value: 1}},
+		})
 
-	// 	// return c.JSON(jobList)
-	// 	return c.SendString("Job Get API")
-	// })
-	// Set up a simple GET endpoint to return the job list TODO Needed to get the data from Any DB
+		// REMOVE IT AFTER TESTING
+		log.Printf("Amount: %s, Checkpoint: %s", amount, checkpoint)
+
+		// return c.JSON(jobList)
+		return c.SendString("Job Get API")
+	})
 
 	app.Post("/job", func(c *fiber.Ctx) error {
 
@@ -79,19 +75,18 @@ func main() {
 
 		//Creating the struct for the job
 		newJob := Job{
-			JobId: uuid.New().String(),
-			Job:  jobDescription,
+			JobId:     uuid.New().String(),
+			Job:       jobDescription,
+			CreatedAt: time.Now(),
 		}
 
 		// Insearting the new job into the MongoDB collection
-		fmt.Printf("%+v\n", newJob)
 		_, err := jobCollection.InsertOne(context.TODO(), newJob)
 		if err != nil {
-			log.Fatalf("Failed to insert job into MongoDB: %v", err)
+			log.Printf("Failed to insert job into MongoDB: %v", err)
 			return c.Status(500).SendString("Failed to insert job into MongoDB")
 		}
 		log.Printf("Inserted job: %v", newJob)
-		// Append the new job to the job list
 		return c.Status(fiber.StatusCreated).JSON(fiber.Map{
 			"message": "Job created successfully",
 			"jobId":   newJob.JobId,
